439fca8dfdd9d72ad0ff58b530a36906cbcbbff9
NataliaSablina <nataliasablina145@gmail.com>
2022-02-11 10:06:54 +0300

    three

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns =[
     path('', Main.as_view(), name='main'),
-    path('home', ViewPost.as_view(), name='home')
+    path('home', ViewPost.as_view(), name='home'),
+    path('like_post/<int:post_id>', like_post, name='like_post')
 ]
--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
-from .models import Post
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, TemplateView, View
+from .models import Post, Like
 
 
-class Main(ListView):
+class Main(TemplateView):
     template_name = 'blog/main_page.html'
 
     def get_queryset(self):
-       pass
+        pass
 
 
 class ViewPost(ListView):
@@ -19,3 +19,14 @@ class ViewPost(ListView):
         return Post.objects.all()
 
 
+def like_post(request, post_id):
+    if request.user.is_authenticated:
+        post = Post.objects.get(pk=post_id)
+        user = request.user
+        try:
+            like = Like.objects.get(post=post, user=user)
+            like.delete()
+        except Like.DoesNotExist:
+            like = Like.objects.create(post=post, user=user)
+
+    return redirect('home')
