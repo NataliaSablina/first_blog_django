23c8daa465f2bfed3ed4c7e1509eacf0b8a72901
NataliaSablina <nataliasablina145@gmail.com>
2022-02-11 10:05:38 +0300

    init2.0

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -8,7 +8,18 @@ class Post(models.Model):
     content = models.TextField(verbose_name='Контент')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="my_posts")
+
+    def get_like_number(self):
+        return self.like_set.all().count()
+
+    def is_liked(self, user):
+        print(self.like_set.filter(user=user))
+        return True
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
 
 
 class Comment(models.Model):
@@ -19,6 +30,7 @@ class Comment(models.Model):
 
 
 class Like(models.Model):
-    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
-    post = models.ForeignKey(Post, on_delete=models.PROTECT)
-    comment = models.ForeignKey(Comment, on_delete=models.PROTECT)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.PROTECT, verbose_name="Пользователь")
+    post = models.ForeignKey(Post, on_delete=models.PROTECT, verbose_name="Пост", blank=True, null=True)
+    comment = models.ForeignKey(Comment, on_delete=models.PROTECT, verbose_name="Комментарий", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата лайка', blank=True, null=True)
